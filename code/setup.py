b482730bd55bef5975e70cc7cc8caa8e7ade9e80
Harshith M R <roovy54@gmail.com>
2024-10-16 17:40:14 +0530

    Update setup.py

--- code/setup.py
+++ code/setup.py
@@ -1,15 +1,10 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name='groove2groove',
     version='0.0.1',  # Add a version for clarity
     author='Ondřej Cífka',
     description='Music style transfer and style translation models',
-    long_description=long_description,
-    long_description_content_type="text/markdown",  # Specify content type for long description
     url='https://github.com/cifkao/groove2groove',
     python_requires='>=3.6',
     install_requires=[
