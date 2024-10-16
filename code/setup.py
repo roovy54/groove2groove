a39c5e8929d7aa3bebf737900b323cbfa009e081
Harshith M R <roovy54@gmail.com>
2024-10-16 17:38:20 +0530

    Update setup.py

--- code/setup.py
+++ code/setup.py
@@ -1,17 +1,28 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='groove2groove',
+    version='0.0.1',  # Add a version for clarity
     author='Ondřej Cífka',
     description='Music style transfer and style translation models',
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Specify content type for long description
     url='https://github.com/cifkao/groove2groove',
     python_requires='>=3.6',
     install_requires=[
-        'scipy'
+        'scipy>=1.2.0'
     ],
     extras_require={
-        'gpu': 'museflow[gpu] @ git+https://github.com/cifkao/museflow',
-        'nogpu': 'museflow[nogpu] @ git+https://github.com/cifkao/museflow',
+        'gpu': ['museflow[gpu] @ git+https://github.com/cifkao/museflow'],
+        'nogpu': ['museflow[nogpu] @ git+https://github.com/cifkao/museflow'],
     },
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Add license info if available
+        'Operating System :: OS Independent',
+    ],
 )
